2aac9843039bbe0c1f7b46c3bd22cc447f1a5d93
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-03-18 10:20:38 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbzeir5BEIVXP46xcQzbjgRUVggVYripPu0hv_9PvGZaLu_81T0UoH3gQPbLeq3Z-pDs/exec";
 
+function login() {
+  const usuario = document.getElementById('usuario').value;
+  const pin = document.getElementById('pin').value;
+
+  fetch(API_URL, {
+    method: 'POST',
+    body: JSON.stringify({
+      action: 'login',
+      usuario: usuario,
+      pin: pin
+    })
+  })
+  .then(res => res.json())
+  .then(data => {
+    if(data.status) {
+      alert('Bienvenido ' + data.usuario);
+    } else {
+      alert('Error de acceso');
+    }
+  });
+}
