3be06270027040341e20d289e703f2d8727512d2
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-03-18 13:43:16 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const API = "https://script.google.com/macros/s/AKfycbw1ivWeBv0Fh6fiM9hQM_x8VXOtLpb_EhJHAxRVYwP5cyxNYBB7iTYFt8wyP5tppLGk/exec";
+const API = "https://script.google.com/macros/s/AKfycbzFDkZEpeRqwduCvayz1mLt8hriDmh3UvAQlxG6RA18HSwRzc8X6yIkOl0KzlYq4WMi/exec";
 
 function login() {
 
