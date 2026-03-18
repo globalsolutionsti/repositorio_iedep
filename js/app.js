89e3f76ada528b9c960a047ad7d43b0ef04ff727
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-03-18 11:16:21 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,24 +1,21 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbz3yMl9gFYvv40j5HyK5QiKNf9TCIMTyd7zdR-9c2ryYvMT8c0NGp7JmtW7VrXs8qH2/exec";
 
 function login() {
-  const usuario = document.getElementById('usuario').value;
-  const pin = document.getElementById('pin').value;
-
-  fetch(API_URL, {
-    method: 'POST',
+  fetch(API, {
+    method: "POST",
     body: JSON.stringify({
-      action: 'login',
-      usuario,
-      pin
+      action: "login",
+      usuario: usuario.value,
+      pin: pin.value
     })
   })
-  .then(res => res.json())
-  .then(data => {
-    if(data.status) {
-      localStorage.setItem('usuario', JSON.stringify(data));
-      window.location.href = 'dashboard.html';
+  .then(r => r.json())
+  .then(d => {
+    if (d.status) {
+      localStorage.setItem("user", JSON.stringify(d));
+      window.location = "dashboard.html";
     } else {
-      alert('Credenciales incorrectas');
+      alert("Error");
     }
   });
 }
