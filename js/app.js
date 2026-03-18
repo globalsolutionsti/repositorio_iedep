b3cad5167a1f169cda541f26d015a5de6e30ac99
globalsolutionsti <54824206+globalsolutionsti@users.noreply.github.com>
2026-03-18 11:18:10 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbz3yMl9gFYvv40j5HyK5QiKNf9TCIMTyd7zdR-9c2ryYvMT8c0NGp7JmtW7VrXs8qH2/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxzSE4pJ4gk3DSF1f9wkdgbgOC18lKC8xeWNT0Y8zvm4HjNENX9_jlQVtOlBDS9DsW0/exec";
 
 function login() {
   fetch(API, {
